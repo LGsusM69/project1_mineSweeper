2ecbbbbca2afe5590324e6a7b35baee63390d2b6
JesusM97 <jissousklp@gmail.com>
2024-02-06 12:30:15 -0700

    layed out script document. added a few variables and event listeners

---
+++ script.js
@@ -0,0 +1,21 @@
+
+//constants and variables
+let difficulty;
+
+//DOM constants
+const newGameButton = document.querySelector("#newGame");
+const playingField = document.querySelector("#playingFiled");
+
+// event listeners
+newGameButton.addEventListener(click, function(evt) {
+
+});
+playingField.addEventListener(click,function(evt) {
+
+    
+});
+//functions
+
+function newGame() {
+
+}

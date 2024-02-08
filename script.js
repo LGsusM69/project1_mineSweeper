6a9aee8d7e152218951c701b0931f2a77f3bd92f
JesusM97 <jissousklp@gmail.com>
2024-02-08 07:36:09 -0700

    implemented the actionListener method for the cells. implemented the logic for cell behavior

--- script.js
+++ script.js
@@ -25,17 +25,29 @@ const cells = document.querySelectorAll(".cell");
 newGameButton.addEventListener("click", function(evt) {
     console.log(evt.target);
 });
+
 playingField.addEventListener("click",function(evt) {
-    if(evt.target.tagName !== "BUTTON") return;
-    if(!gameRunning) return;
+    if(evt.target.tagName !== "BUTTON") return;  //make sure its a button
+    if(!gameRunning) return;            // game is running?
     const index = evt.target.parentElement.id.slice(1);
-    if(cellState[index] === "x") {
+    if(cellState[index] === "x") {      //check if its a mine
         gameLost(index);
         return;
     }
+    //if(cellState[index].class) return;
+    console.log(cellState[index] + "calamardo:  ");
+
     const adjacentMines = calculateAdjacent(index);
     if(adjacentMines === 0) {
         cells[index].innerHTML = "";
+    }
+    else {
+        const newH1 = document.createElement("h1");
+        newH1.style.width = "100%";
+        newH1.style.height = "100%";
+        newH1.innerText = adjacentMines;
+        cells[index].innerHTML = "";
+        cells[index].appendChild(newH1);
     }
 
 });
@@ -59,23 +71,25 @@ function newGame() {
         if(element === "x") return;
         cellState[index] = calculateAdjacent(index);
     });
-    cellState.forEach(element => {
+    cellState.forEach(element => {      // update the mine counter
         if(element === "x")mineCounter.value ++
         
     });
-    cells.forEach(function(element, index) {
-        element.style.backgroundColor = "lightgray";
+    cells.forEach(function(element, index) {        //populate cells with buttons
         const newButton = document.createElement("button");
         newButton.style.width = "100%";
         newButton.style.height = "100%";
         newButton.style.background = "darkgray";
+        cells[index].innerHTML = "";
         cells[index].appendChild(newButton);
         //cells[index].innerHTML = "<button></button>";
         //console.log(element.innerHTML === cells[0].innerHTML);
         gameRunning = true;
-        console.log(gameRunning);
+        //console.log(gameRunning);
+
     });
     //cells[0].innerHTML = "calamardo";
+    console.log(cellState);
 }
 function gameLost(index) {
 
@@ -83,4 +97,38 @@ function gameLost(index) {
 function gameWon() {
 
 }
-function calculateAdjacent(index) {return 0;}
+function calculateAdjacent(index) {
+    const adjacentCells = new Set();
+    let adjacentMines = 0;
+    adjacentCells.add(index-11);
+    adjacentCells.add(index-10);
+    adjacentCells.add(index-9);
+    adjacentCells.add(index-1);
+    adjacentCells.add(index+1);
+    adjacentCells.add(index+9);
+    adjacentCells.add(index+10);
+    adjacentCells.add(index+11);
+    if(index%10 === 0) {
+        adjacentCells.delete(index-11);
+        adjacentCells.delete(index-1);
+        adjacentCells.delete(index+9);
+    }
+    if(index%10 === 9) {
+        adjacentCells.delete(index-9);
+        adjacentCells.delete(index+1);
+        adjacentCells.delete(index+11);
+    }
+    if(index < 10) {
+        adjacentCells.delete(index-11);
+        adjacentCells.delete(index-10);
+        adjacentCells.delete(index-9);
+    }
+    if(index > 89) {
+        adjacentCells.delete(index+9);
+        adjacentCells.delete(index+10);
+        adjacentCells.delete(index+11);
+    }
+    for(const value of adjacentCells) {
+        if(cellState[value] === "x") adjacentMines ++;
+    }
+    return adjacentMines;}
